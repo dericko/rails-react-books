da52e6abe866c8dd91be0961d2f9fb201747994c
Derick Olson <olsonderick@gmail.com>
2023-05-24 18:02:44 -0400

    setup react on rails

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>React on Rails</h1>
+    </div>
+  );
+};
+
+
+// TODO: see test-rails-app and re-create the books API listing
+
+export default App;
