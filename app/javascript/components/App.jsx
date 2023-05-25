6b065c663f221fb7f666ea27ca2c63e236eba0d4
Derick Olson <olsonderick@gmail.com>
2023-05-25 17:35:44 -0400

    call books api in react

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,24 +1,45 @@
-import React from "react";
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-const App = () => {
+// const BOOKS_ENDPOINT = `${process.env.REACT_APP_API_URL}/nyt_books`;
+const BOOKS_ENDPOINT = `http://localhost:3000/api/nyt_books`;
+
+const Books = () => {
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await fetch(BOOKS_ENDPOINT);
+        const data = await response.json();
+        setBooks(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchBooks();
+  }, []);
+
   return (
-    <div className="App">
-      <h1>React on Rails</h1>
-      <h2>TODO:</h2>
-      <ul>
-        <li>See test-rails-app and re-create the books API listing</li>
-        <li>first create the api controller to fetch from nyt</li>
-        <li>will require dotenv-rails</li>
-        <li>then tell component to fetch from api</li>
-        <li>figure out how to use js dotenv, same file?</li>
-        <li>then can go ahead and look at EXP RN project!</li>
-      </ul>
+    <div className="BooksContainer">
+      <h1>Books</h1>
+      {books.map((book) => (
+        <a className="Book" key={book.id} href={book.link}>
+          <img style={{ width: 75 }} src={book.imageUrl} alt={book.title} />
+          <p>{book.title}</p>
+          <p>{book.author}</p>
+          <p>{book.description}</p>
+        </a>
+      ))}
     </div>
   );
 };
 
-
-// TODO: see test-rails-app and re-create the books API listing
+const App = () => {
+  return (
+    <React.StrictMode>
+      <Books />
+    </React.StrictMode>
+  );
+};
 
 export default App;
